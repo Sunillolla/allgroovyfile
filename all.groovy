60eba37cc5d7035c75e1ad6d8cf7b0be0b8e4d18
Sunillolla <134775756+Sunillolla@users.noreply.github.com>
2023-07-21 15:59:35 +0530

    Update all.groovy

--- all.groovy
+++ all.groovy
@@ -2,9 +2,16 @@ pipeline {
     agent any
     tools {
         maven 'Maven-3.8.8'
+        jdk 'JDK17'
     }
     environment {
         TOMCAT_CREDS = credentials('tomcat_credentials') // jenkins credentials
+        NEXUS_VERSION = "nexus3"
+        NEXUS_PROTOCOL = "http"
+        NEXUS_URL= "34.125.75.196:8081"
+       // NEXUS_REPO= "allrepo-releases"
+       NEXUS_REPO= "new-repo-test"
+
     }
     stages {
         stage ('clone') {
@@ -33,8 +40,40 @@ pipeline {
                 echo "echo ${files[0].name} ${files[0].path} ${files[0].directory} ${files[0].length} ${files[0].lastModified}"
                 artifactPath = files[0].path;
                 artifactExists = fileExists artifactPath;
-                echo "If artifact exists"
+                // echo "If artifact exists"
                 echo "${artifactExists}"
+
+                if (artifactExists) {
+                    echo "********** Artifact is available, going to deploy to nexus"
+                    echo "File is : ${artifactPath} , Package is : ${pom.packaging} , Version is : ${pom.version} , GroupID is : ${pom.groupId}"
+                    // we need to deploy to nexus , using a plugin called as nexusArtifactuploader
+                    nexusArtifactUploader (
+                        nexusVersion: "$env.NEXUS_VERSION",
+                        protocol: "${env.NEXUS_PROTOCOL}",
+                        nexusUrl: "${env.NEXUS_URL}", // env. | params. | pipelineparams.
+                        groupId: "${pom.groupId}",
+                        version: "${BUILD_NUMBER}",
+                        repository: "${env.NEXUS_REPO}",
+                        credentialsId: "nexus_creds",
+                          artifacts: [
+                            [
+                                artifactId: pom.artifactId,
+                                type: pom.packaging,
+                                classifier:'',
+                                file: artifactPath,
+
+                            ]
+                            
+
+                          ]
+
+                    )
+                        
+                    }
+                
+                else {
+                    error "********** ${artifactPath} is not available ********** "
+                }
                 }
             }
 
@@ -42,7 +81,7 @@ pipeline {
         stage ('Deploy to tomcat') {
             steps {
                 // curl commands 
-                sh "curl -v -u ${TOMCAT_CREDS_USR}:${TOMCAT_CREDS_PSW} -T /var/lib/jenkins/workspace/allpipeline/target/spring3-mvc-maven-xml-hello-world-1.0-SNAPSHOT.war 'http://34.16.170.223:8085/manager/text/deploy?path=/spring-hello'"
+                sh "curl -v -u ${TOMCAT_CREDS_USR}:${TOMCAT_CREDS_PSW} -T /var/lib/jenkins/workspace/allpipeline/target/spring3-mvc-maven-xml-hello-world-1.0-SNAPSHOT.war 'http://34.125.127.26:8085//manager/text/deploy?path=/spring-hello&update=true'"
             }
         }
     }
